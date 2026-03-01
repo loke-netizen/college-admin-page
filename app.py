b67bfca9cad9a32dd82235f370c811b481f2df15
LOKESH <codelearning2846@gmail.com>
2026-03-01 22:10:46 +0530

    alert msg for invalid user

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 app=Flask(__name__)
+ADMIN_USERNAME = "admin"
+ADMIN_PASSWORD = "1234"
+
 @app.route("/")
 def login():
     return render_template("login.html")
-@app.route("/homepage")
+
+@app.route("/homepage", methods=["POST"])
 def home():
-   return render_template("index.html")
+    username = request.form.get("username")
+    password = request.form.get("password")
+
+    if username == ADMIN_USERNAME and password == ADMIN_PASSWORD:
+        return render_template("index.html")
+    else:
+        return render_template("login.html", error="Invalid Username or Password")
+
 if __name__=="__main__":
     app.run(debug=True)
