f6b32cd15389222f66c47c5804db7da848a4335d
lokesh <codelearning2846@gmail.com>
2026-03-03 20:37:41 +0530

    remove the unwanted lines

--- app.py
+++ app.py
@@ -3,15 +3,10 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-# DATABASE CONFIG
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///college.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
-
-# ================= MODELS =================
-
-# ================= MODELS =================
 
 class Department(db.Model):
     dept_id = db.Column(db.Integer, primary_key=True)
